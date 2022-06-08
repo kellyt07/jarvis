e26a6a3df4aa5a79e5fc6371463ee883c6d262cd
kellyt07 <36403053+kellyt07@users.noreply.github.com>
2022-06-08 15:36:46 +1000

    UI changes

--- jarvisui/src/App.js
+++ jarvisui/src/App.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import TextareaAutosize  from '@material-ui/core/TextareaAutosize';
-import { Button } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import './App.css';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="titleheader">JARVIS</div>
-        <div className="textinput">
-          <TextareaAutosize
-              aria-label="minimum height"
-              minRows={20}
-              placeholder="Enter Text to extract"
-              style={{ width: 500 }}
-            />
-            <Button 
-            variant="contained"
-            onClick={() => {
-              alert('clicked');
-              }}>
-              Assess</Button>
+        <div className="titleheader">
+            <Box sx={{ flexGrow: 1 }}>
+              <AppBar position="static" style={{ background: 'black' }}>
+                <Toolbar>
+                  <img src={require('./jarvis_image.png')} alt="jarvis" width="100" height="100"></img>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    JARVIS
+                  </Typography>
+                 </Toolbar>
+              </AppBar>
+            </Box>
         </div>
-        <div className="entities">
-            Entities Extracted
+        <div className="inputpanel">
+          <Card sx={{ minWidth: 20 }} variant="outlined">
+            <CardContent>
+              <div className="textarea">
+                <TextareaAutosize
+                  aria-label="minimum height"
+                  minRows={10}
+                  placeholder="Enter Text to extract"
+                  style={{ width: 600 }}
+                />
+              </div>
+              <div className="inputbutton">
+                <Button variant="contained">
+                  Assess</Button>
+                </div>
+                <div className="textarea">
+                <TextareaAutosize
+                  aria-label="minimum height"
+                  minRows={10}
+                  placeholder="Enter Text to extract"
+                  style={{ width: 600 }}
+                />
+              </div>
+            </CardContent>
+          </Card>
+          </div>
+          <div className="resultpanel">
+          <Card sx={{ minWidth: 20 }} variant="outlined">
+            <CardContent>
+              This is the risk
+            </CardContent>
+          </Card>
           </div>
       </div>
     );
