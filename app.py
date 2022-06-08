31e80aff60843404cd9aebef80350a0af5ebae1d
Callum Robertson <callumr1@users.noreply.github.com>
2022-06-08 13:10:29 +1000

    Add api param pass through
    Changed api endpoint to pass through the text parameter to jarvis

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import flask
 import flask_restful
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-
+from flask import Flask, request
+from flask_restful import Resource, Api
 from jarvis import Jarvis
 
 app = Flask(__name__)
@@ -10,18 +9,12 @@ api = Api(app)
 
 class GetKeyWords(Resource):
     def get(self):
-        text = ("A Chinese intelligence ship was tracked off Australia's west coast within 50 nautical miles of a sensitive defence facility, Australia said on Friday, raising concern amid an election campaign about China's behaviour in the region."
-                "Prime Minister Scott Morrison said the Chinese navy vessel was not in Australian territorial waters but its presence was 'concerning'."
-                "'It is clearly an intelligence ship and they are looking at us and we're keeping a close eye on them,' he told reporters.'")
-        return Jarvis.getKeyWords(text)
+        return Jarvis.getKeyWords(request.args.get("text"))
     pass
 
 class GetKeyWordsHtml(Resource):
     def get(self):
-        text = ("A Chinese intelligence ship was tracked off Australia's west coast within 50 nautical miles of a sensitive defence facility, Australia said on Friday, raising concern amid an election campaign about China's behaviour in the region."
-                "Prime Minister Scott Morrison said the Chinese navy vessel was not in Australian territorial waters but its presence was 'concerning'."
-                "'It is clearly an intelligence ship and they are looking at us and we're keeping a close eye on them,' he told reporters.'")
-        return Jarvis.getKeyWordsHtml(text)
+        return Jarvis.getKeyWordsHtml(request.args.get("text"))
 
 api.add_resource(GetKeyWords, '/jarvis/getkeywords')
 api.add_resource(GetKeyWordsHtml, '/jarvis/getkeywordshtml')
--- jarvis.py
+++ jarvis.py
@@ -8,23 +8,18 @@ class Jarvis:
     def getKeyWords(text):
         # Load the English tokenizer, tagger, parser and NER
         nlp = spacy.load("en_core_web_sm")
-
         doc = nlp(text)
 
         # initialise dictionary that will be passed back
         words = {}
-
         # Add word and its label/explaination to dict
         for word in doc.ents:
             words[word.text] = word.label_ + " - " + spacy.explain(word.label_)
-            print (word)
-
         return words
 
     def getKeyWordsHtml(text):
         # Load the English tokenizer, tagger, parser and NER
         nlp = spacy.load("en_core_web_sm")
-
         doc = nlp(text)
 
         return displacy.render(doc, style="ent")
