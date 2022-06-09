4e8598b141be434d4edf513ee3fcad0ed977dd52
kellyt07 <36403053+kellyt07@users.noreply.github.com>
2022-06-09 14:10:36 +1000

    Update Strategy.js

--- jarvisui/src/Strategy.js
+++ jarvisui/src/Strategy.js
@@ -28,11 +28,11 @@ export function Strategy() {
             {
                 type: "column",
                 dataPoints: [
-                    { label: "Apple",  y: 10  },
-                    { label: "Orange", y: 15  },
-                    { label: "Banana", y: 25  },
-                    { label: "Mango",  y: 30  },
-                    { label: "Grape",  y: 28  }
+                    { label: "January",  y: 10  },
+                    { label: "February", y: 15  },
+                    { label: "March", y: 25  },
+                    { label: "April",  y: 0  },
+                    { label: "May",  y: 28  }
                 ]
             }
             ]
